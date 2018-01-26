c544f031aea919720df0966b515306fb0b9afb7f
Bryan Geedman <bgeedman@gmail.com>
2018-01-25 20:17:34 -0600

    Modified to simulation to parse a command string.  THIS IS NOT WORKING CURRENTLY BUT I DON't CARE

--- tools/simulation.py
+++ tools/simulation.py
@@ -5,6 +5,9 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from math import cos, sin, radians, degrees
 import numpy as np
+import sys
+import re
+
 
 def _cos(deg):
     return np.cos(np.radians(deg))
@@ -65,11 +68,57 @@ def forward_kinematics(coxa, femur, tibia, theta, alpha, beta):
 
 
 def animate(i):
-    data = np.loadtxt("../src/foobar")
-    forward_kinematics(*data)
+    data = _file.readline()
+    if (data):
+        print data
+        parse_command_string(data)
 
 
+#define FRONT_LEFT_COXA                     53
+#define FRONT_LEFT_FEMUR                    78
+#define FRONT_LEFT_TIBIA                    122
 
+def parse_command_string(buf):
+    '''
+        This is what a sample command looks like.
+            #27 P1115 #28 P2157 #29 P1885 #11 P2030 #12 P1052 #13 P1284 #20 P1956 \
+            #19 P992 #18 P1254 #4 P1075 #3 P2157 #2 P1875 T1000
+
+        #<number> identifies the pin
+        P<number> identifies the servo position
+        T<number> identifies the time to take to get that position
+
+        So need to parse the string and convert the position to an angle for the
+        forward kinematics function
+    '''
+    ranges = [[[1115, 2015], [90.0, 0.0]],
+              [[650, 2500], [90.0, -90.0]],
+              [[740, 2275], [0.0, 180.0]],
+              [[1130, 2030], [90.0, 0.0]],
+              [[710, 2500], [-90.0, 90.0]],
+              [[850, 2430], [180.0, 90.0]],
+              [[1056, 1956], [90.0, 0.0]],
+              [[650, 2465], [-90.0, 90.0]],
+              [[815, 2400], [180.0, 0.0]],
+              [[1075, 1975], [90.0, 0.0]],
+              [[635, 2500], [90.0, -90.0]],
+              [[730, 2215], [0.0, 180.0]]]
+
+    args = []
+    for index,x in enumerate(re.findall(r'#\d+ P\d+', buf)):
+        y = [int(a) for a in re.findall(r'\d+', x)]
+        pin = y[0]
+        position = y[1]
+        args.append(np.interp(position, ranges[index][0], ranges[index][1]))
+    print args
+    forward_kinematics(53, 78, 122, args[0], args[1], args[2])
+
+
+try:
+    _file = open("../src/foobar", "r")
+except:
+    print "Failed to open data file"
+    sys.exit(1)
 
 fig = plt.figure()
 ax1 = fig.add_subplot(111, projection='3d')
@@ -81,4 +130,3 @@ ax1.set_ylabel('y axis')
 ax1.set_zlabel('z axis')
 ani = animation.FuncAnimation(fig, animate, interval=1000)
 plt.show()
-
